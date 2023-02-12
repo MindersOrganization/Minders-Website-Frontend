2a1f2f60a377c6b3be950ebb1ba9126d28b501a2
AbdelrahmanAli-gif <abdelrahmanali58@gmail.com>
2023-02-13 00:42:08 +0200

    magazine section & fixed navbar-footer

---
+++ Minders/scripts/root.js
@@ -0,0 +1,13 @@
+const hamburgerMenu = document.getElementsByClassName('hamburger-icon')[0];
+const navbarList = document.getElementsByClassName('mobile-menu')[0];
+
+function displayNavbar() {
+    if (navbarList.style.visibility == 'hidden'){
+        navbarList.style.visibility = 'visible';
+        hamburgerMenu.style.backgroundImage = 'url("./media/icons8-close-window-100.png")'
+    }
+    else{
+        navbarList.style.visibility = 'hidden'
+        hamburgerMenu.style.backgroundImage = 'url("../media/icons8-menu-squared-100.png")'
+    }
+}
