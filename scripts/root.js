cae793cb38bdf37a072feed0a062f88d7899d1df
AbdelrahmanAli-gif <abdelrahmanali58@gmail.com>
2023-04-18 02:10:11 +0200

    fixed some bugs

--- scripts/root.js
+++ scripts/root.js
@@ -1,13 +1,26 @@
 const hamburgerMenu = document.getElementsByClassName('hamburger-icon')[0];
 const navbarList = document.getElementsByClassName('mobile-menu')[0];
+const activePage = document.getElementsByClassName('active');
 
 function displayNavbar() {
-    if (navbarList.style.visibility == 'hidden'){
-        navbarList.style.visibility = 'visible';
-        hamburgerMenu.style.backgroundImage = 'url("../media/icons8-close-window-100.png")'
+    if (activePage.length > 0){
+        if (navbarList.style.visibility == 'hidden'){
+            navbarList.style.visibility = 'visible';
+            hamburgerMenu.style.backgroundImage = 'url("../media/icons8-close-window-100.png")'
+        }
+        else{
+            navbarList.style.visibility = 'hidden'
+            hamburgerMenu.style.backgroundImage = 'url("../media/icons8-menu-squared-100.png")'
+        }
     }
     else{
-        navbarList.style.visibility = 'hidden'
-        hamburgerMenu.style.backgroundImage = 'url("../media/icons8-menu-squared-100.png")'
+        if (navbarList.style.visibility == 'hidden'){
+            navbarList.style.visibility = 'visible';
+            hamburgerMenu.style.backgroundImage = 'url("./media/icons8-close-window-100.png")'
+        }
+        else{
+            navbarList.style.visibility = 'hidden'
+            hamburgerMenu.style.backgroundImage = 'url("./media/icons8-menu-squared-100.png")'
+        }
     }
 }
