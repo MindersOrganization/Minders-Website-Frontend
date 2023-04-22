4d9135ab29218b27eaca8887239c7944106e8db5
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2023-04-23 00:10:30 +0200

    re-written the event-list + fixed card height

--- scripts/event-list.js
+++ scripts/event-list.js
@@ -1,7 +1,12 @@
-import BASE_URL from "./root.js";
+import { BASE_URL } from "./root.js";
+
+/**
+ * TODO dividing events into sections according to year
+ */
 
 const cardsContainer = document.querySelector(".event-cards-container");
 
+/** changes the value of status from shorthand to full status name */
 const setStatus = (statusVal) => {
   let status;
   switch (statusVal) {
@@ -12,45 +17,61 @@ const setStatus = (statusVal) => {
       status = "finished";
       break;
     case "u":
-      status = "to-be-done";
+      status = "upcoming";
+      break;
+    case "po":
+      status = "postponed";
+      break;
+    case "s":
+      status = "sold-out";
       break;
   }
 
   return status;
 };
 
-const getEvents = () => {
+/** gets all the events in the data base and
+ * @returns {Object[]} a list of events { title, status, card_img, date }
+ */
+const getEvents = async () => {
   const eventsData = [];
-  axios
-    .get(`${BASE_URL}/events/`)
-    .then((res) => {
-      for (let i = 0; i < res.data.length; i++) {
-        eventsData.push({
-          title: res.data[i].title,
-          status: setStatus(res.data[i].status),
-          card_img: BASE_URL + res.data[i].list_img,
-          date: res.data[i].date,
-        });
-      }
-      eventsData.forEach((val) => {
-        let eventCard = `
-        <div class="event-card">
-          <div class="status-tag ${val.status}">
-            <span class="status-circle ${val.status}"></span>
-            <div class="status-name">${val.status}</div>
-          </div>
-          <img
-            src="${val.card_img}"
-            alt=""
-            class="event-img"
-          />
-          <div class="event-title text-center">${val.title}</div>
-        </div>
-        `;
-        cardsContainer.innerHTML += eventCard;
-      });
-    })
-    .catch((e) => console.log(e));
+  const res = await axios.get(`${BASE_URL}/events/`);
+  res.data.forEach((val) => {
+    eventsData.push({
+      id: val.id,
+      title: val.title,
+      status: setStatus(val.status),
+      card_img: BASE_URL + val.list_img,
+      date: val.date,
+    });
+  });
+  return eventsData;
 };
 
-getEvents();
+/** displays events on the page */
+const displayEvents = async () => {
+  const events = await getEvents();
+  events.forEach((val) => {
+    let eventCard = `
+    <div class="event-card">
+      <div class="status-tag ${val.status}">
+        <span class="status-circle ${val.status}"></span>
+        <div class="status-name">${
+          val.status === "sold-out" ? "sold out" : val.status
+        }</div>
+      </div>
+      <div class = "event-img-container">
+        <img
+        src="${val.card_img}"
+        alt=""
+        class="event-img"
+        />
+      </div>
+      <div class="event-title text-center" /><a class="" href = "#">${val.title}<a></div>
+    </div>
+    `;
+    cardsContainer.innerHTML += eventCard;
+  });
+};
+
+displayEvents();
