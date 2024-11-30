4694345f100579058a28b361627e5b34386029a5
DELL <salma.a.samei@gmail.com>
2024-11-30 20:01:46 +0200

    added hiring section updated the function of contact section added event card

--- scripts/sendMessage.js
+++ scripts/sendMessage.js
@@ -1,32 +1,62 @@
-import { BASE_URL } from "./displayMobileNavbar.js";
+// import { BASE_URL } from "./displayMobileNavbar.js";
 
-const REQUIRED_URL = BASE_URL + '/user/messages/';
+// const REQUIRED_URL = BASE_URL + '/user/messages/';
 
-const form = document.getElementsByClassName('form-content')[0];
-const userName = document.getElementsByClassName('name-input')[0];
-const userEmail = document.getElementsByClassName('email-input')[0];
-const userSubject = document.getElementsByClassName('subject-input')[0];
-const userMessage = document.getElementsByClassName('message-input')[0];
-const messageType = document.getElementsByClassName('dropdown-values')[0];
-const submitButton = document.getElementsByClassName('form-submit')[0];
+// const form = document.getElementsByClassName('form-content')[0];
+// const userName = document.getElementsByClassName('name-input')[0];
+// const userEmail = document.getElementsByClassName('email-input')[0];
+// const userSubject = document.getElementsByClassName('subject-input')[0];
+// const userMessage = document.getElementsByClassName('message-input')[0];
+// const messageType = document.getElementsByClassName('dropdown-values')[0];
+// const submitButton = document.getElementsByClassName('form-submit')[0];
 
-form.addEventListener('submit', handleSubmit);
+// submitButton.addEventListener('click', sendEmail);
 
-function handleSubmit(e) {
-    e.preventDefault();
-    axios
-        .post(REQUIRED_URL,
-            {
-                name: userName.value,
-                email: userEmail.value,
-                subject: userSubject.value,
-                message: userMessage.value,
-                type: messageType.value
-            })
-        .then((res) => {
-            window.location.assign('../pages/contact-success.html');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+// function handleSubmit(e) {
+// e.preventDefault();
+// axios
+//     .post(REQUIRED_URL,
+//         {
+//             name: userName.value,
+//             email: userEmail.value,
+//             subject: userSubject.value,
+//             message: userMessage.value,
+//             type: messageType.value
+//         })
+//     .then((res) => {
+//         window.location.assign('../pages/contact-success.html');
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// }
+
+function sendMail(){
+    
+    var params={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+        type:document.getElementById("type").value,
+        subject:document.getElementById("subject").value,
+    };
+
+const serviceID="service_nu760rh";
+const templateID="template_x448dxf";
+
+emailjs.send(serviceID,templateID,params)
+.then(res=>{
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("message").value="";
+    document.getElementById("subject").value="";
+    document.getElementById("type").value="BUG";
+
+    console.log(res);
+alert("your message sent successfully");
+})
+.catch(err=>{
+    alert("something wrong happened");
+    console.log(err)
+})
 }
